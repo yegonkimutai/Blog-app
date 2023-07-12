6907e14e505b876ab71a8d4a59f77eaea0ec250b
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-12 14:36:21 +0300

    :sparkles:Add associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :post_id
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
+
+  has_many :likes, dependent: :destroy
 end
