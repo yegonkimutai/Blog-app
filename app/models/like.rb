82a97a00b9ccc8eb63b5dd9e83c31e9c364ea966
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-12 15:22:50 +0300

    :art:Update counters and methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  after_save :likes_update
+
   private
 
   def likes_update
