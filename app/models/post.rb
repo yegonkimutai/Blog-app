d0d4447c0820fbf65c1e26e0fbe1490ed93b5dcd
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-12 16:37:17 +0300

    :art:Improve files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
-  has_many :likes, class_name: 'Like', dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   after_save :update_posts
 
