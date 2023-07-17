02f0d344b9ea744c8186065ed0a82445a9e47340
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-17 20:02:09 +0300

    :hammer:Fix linters

--- spec/controllers/users_contoller_spec.rb
+++ spec/controllers/users_contoller_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
-    describe 'Get users#index' do
-        it 'returns successful response' do
-            get users_path
-            expect(response).to have_http_status(:success)
-        end
-
-        it 'renders template' do
-            get users_path
-            expect(response).to render_template(:index)
-        end
-
-        it 'displays template' do
-            get users_path
-            expect(response.body).to include('Users Available')
-        end
-    end
-
-    describe 'Get users#show' do
-    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-
+  describe 'Get users#index' do
     it 'returns successful response' do
-        get "/users/#{user.id}"
-        expect(response).to have_http_status(:success)
+      get users_path
+      expect(response).to have_http_status(:success)
     end
 
     it 'renders template' do
-        get "/users/#{user.id}"
-        expect(response).to render_template(:show)
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'displays template' do
+      get users_path
+      expect(response.body).to include('Users Available')
+    end
+  end
+
+  describe 'Get users#show' do
+    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+    it 'returns successful response' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
     end
 
 
     it 'displays template' do
-        get "/users/#{user.id}"
-        expect(response.body).to include('Users Available')
+      get "/users/#{user.id}"
+      expect(response.body).to include('Users Available')
     end
-end
+  end
 end
--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   let(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'New user') }
-  let(:post) {  Post.new(author: user, title: 'Nature', text: 'This is exciting!') }
+  let(:post) { Post.new(author: user, title: 'Nature', text: 'This is exciting!') }
   subject { Comment.new(text: 'This is my comment', post:, author: user) }
   before { subject.save }
 
