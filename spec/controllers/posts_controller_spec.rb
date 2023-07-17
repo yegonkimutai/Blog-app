9a38c16dcb9882b8a27ec1f65f14dd9c515cf516
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-17 20:12:18 +0300

    :wrench:Fix linters

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe PostsController, type: :request do
   end
 
   describe 'Get posts#show' do
-    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+    let(:user) { User.create(name: 'Trevor', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
     let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
 
     it 'returns successful response' do
