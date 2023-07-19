f0e51047cf2b7400e2ec0bf7f8240a62a023e381
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-19 18:19:54 +0300

    :wrench:Fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,26 @@
 class CommentsController < ApplicationController
-    def new
-        @user = User.find(params[:user_id])
-        @post = @user.posts.find(params[:post_id])
-        @comment = Comment.new
-    end
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = Comment.new
+  end
 
-    def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.build(comment_params)
-        @comment.author = current_user
-    
-        if @comment.save
-          redirect_to user_post_path(@post.id, @post)
-        else
-          # Handle errors if the comment cannot be saved
-          render 'posts/show'
-        end
-      end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
 
-    private
-    def comment_params
-        params.require(:comment).permit(:text)
+    if @comment.save
+      redirect_to user_post_path(@post.id, @post)
+    else
+      # Handle errors if the comment cannot be saved
+      render 'posts/show'
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
-    def index
-        @likes = Like.all
-      end
-    
-    def create
-        @post = Post.find(params[:post_id])
-        @user = current_user
-        @like = Like.new(author_id: @user.id, post_id: @post.id)
-        return unless @like.save
+  def index
+    @likes = Like.all
+  end
 
-        redirect_to user_posts_path(@post.id, @post)
-    end
+  def create
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    @like = Like.new(author_id: @user.id, post_id: @post.id)
+    return unless @like.save
+
+    redirect_to user_posts_path(@post.id, @post)
+  end
 end
