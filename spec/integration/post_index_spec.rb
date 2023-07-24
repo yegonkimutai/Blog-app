67cd9ae763e21c1f045bdc0d416323168d8bb2db
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-24 22:53:14 +0300

    :hammer:Fix linters

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -1,31 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature 'Post index page' do
+RSpec.feature 'Post page' do
   before do
     @user = User.create(name: 'user1', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Bio 1')
     @post1 = Post.create(author: @user, title: 'Post 1', text: 'Body of post 1')
-    @post2 = Post.create(author: @user, title: 'Post 2', text: 'Body of post 2')
     @comment1 = Comment.create(post: @post1, author: @user, text: 'Comment 1 on Post 1')
     @comment2 = Comment.create(post: @post1, author: @user, text: 'Comment 2 on Post 1')
 
     visit user_posts_path(@user)
   end
 
-  scenario "See the user's profile picture" do
+  scenario "See the user's post" do
     expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']")
-  end
-
-  scenario "I can see the user's username" do
     expect(page).to have_content(@user.name)
-  end
-
-  scenario 'See the number of posts the user has written' do
     expect(page).to have_content(@user.posts.count)
-  end
-
-  scenario "See a post's title" do
     expect(page).to have_content(@post1.title)
-    expect(page).to have_content(@post2.title)
+    expect(page).to have_content(@post1.text)
   end
 
   scenario 'See the first comments on a post' do
@@ -33,18 +23,9 @@ RSpec.feature 'Post index page' do
     expect(page).to have_content(@comment2.text)
   end
 
-  scenario 'See how many comments a post has' do
+  scenario 'See how many comments and likes a post has' do
     expect(page).to have_content(@post1.comments.count)
-  end
-
-  scenario 'I can see how many likes a post has' do
-    likes_count = @post1.likes.count
-    expect(page).to have_content("Likes: #{likes_count}")
-  end
-
-  scenario "I can see some of the post's body" do
-    expect(page).to have_content(@post1.text)
-    expect(page).to have_content(@post2.text)
+    expect(page).to have_content(@post1.likes.count)
   end
 
   scenario 'I can see a section for pagination if there are more posts than fit on the view' do
