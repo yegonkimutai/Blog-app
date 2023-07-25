eb13c3f44b6a4c518b98d409c7ee2f998996130c
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-25 20:05:18 +0300

    :zap:Implement changes requested

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.example.com',
+    port: 587,
+    user_name: 'yegonkimutai.99@gmail.com',
+    password: 'yegonbrian@99',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
